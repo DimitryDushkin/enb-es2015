f90797325162ed2206e71f2a77350d5bc52c2f2d
Dmitry Dushkin <dndushkin@yandex-team.ru>
2016-12-07 19:05:47 +0300

    remove source maps

--- techs/es2015.js
+++ techs/es2015.js
@@ -1,9 +1,9 @@
 // Partial copy of https://github.com/enb-make/enb-js/blob/master/techs/browser-js.js
 var vow = require('vow'),
     enb = require('enb'),
+    fs = require('fs'),
     vfs = enb.asyncFS || require('enb/lib/fs/async-fs'),
     buildFlow = enb.buildFlow || require('enb/lib/build-flow'),
-    File = require('enb-source-map/lib/file'),
     babel = require('babel-core'),
     babelOptions = {
         plugins: [
@@ -30,21 +30,21 @@ module.exports = buildFlow.create()
         return vow.all(promises)
             .spread(function(sources) {
                 var node = this.node,
-                    file = new File(node.resolvePath(this._target), { sourceMap: false });
+                    file = '';
 
                 for (var i = 0; i < sources.length; i++) {
-                    file.writeLine('/* begin: ' + sources[i].relPath + ' */');
+                    file  += '/* begin: ' + sources[i].relPath + ' */\n';
 
                     if (sources[i].relPath.indexOf('/libs/') !== -1) {
-                        file.writeFileContent(sources[i].relPath, sources[i].contents);
+                        file += sources[i].contents;
                     } else {
-                        file.writeFileContent(sources[i].relPath, babel.transform(sources[i].contents, babelOptions).code);
+                        file += babel.transform(sources[i].contents, babelOptions).code;
                     }
 
-                    file.writeLine('/* end: ' + sources[i].relPath + ' */');
+                    file += '/* end: ' + sources[i].relPath + ' */\n';
                 }
 
-                return file.getContent();
+                return file;
             }, this);
     })
     .methods({
